386bae9ff7e2d22528c59eedbb5d3e347158a65b
Josue Eliasaf Plata Garcia <josue.plata@globant.com>
2021-08-02 10:41:14 -0500

    feat: Gradle configs

--- sample/src/main/java/com/josue/sample/Controller/HotelController.java
+++ sample/src/main/java/com/josue/sample/Controller/HotelController.java
@@ -1,6 +1,6 @@
 package com.josue.sample.Controller;
 
-import com.josue.sample.Service.Test;
+import com.josue.sample.Service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("test")
-public class TestController {
+@RequestMapping("hotel")
+public class HotelController {
 
     @Autowired
-    private Test test;
+    private HotelService hotelService;
 
     @GetMapping("/test")
     public void test(){
-        test.Test();
+        hotelService.Test();
     }
 }
--- sample/src/main/java/com/josue/sample/Service/HotelService.java
+++ sample/src/main/java/com/josue/sample/Service/HotelService.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Service;
 
 @CommonsLog
 @Service
-public class Test {
+public class HotelService {
     @Autowired
     private BitacoraRepository bitacoraRepository;
 
-    public Test(BitacoraRepository bitacoraRepository){
+    public HotelService(BitacoraRepository bitacoraRepository){
         this.bitacoraRepository =  bitacoraRepository;
     }
 
